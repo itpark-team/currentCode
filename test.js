cdb48e92eb99590f859bdf4d23ffcb7dfe9a9f94
itpark-team <46758840+itpark-team@users.noreply.github.com>
2020-12-27 21:28:43 +0300

    1111

--- test.js
+++ test.js
@@ -1,2 +1,27 @@
 var a = 123;
-console.log(a);
+var b = 'hello';
+var c = [1, 3345, 4, 6234, 7];
+var d = [1, 4, 56];
+var aa;
+var f = true;
+var Car = /** @class */ (function () {
+    function Car(speed, mark) {
+        this.speed = speed;
+        this.mark = mark;
+    }
+    Car.prototype.printInfo = function () {
+        console.log("mark = " + this.mark + " speed = " + this.speed);
+    };
+    Car.prototype.setSpeed = function (speed) {
+        this.speed = speed;
+    };
+    Car.prototype.setMark = function (mark) {
+        this.mark = mark;
+    };
+    return Car;
+}());
+var car1 = new Car(140, "ford mustang");
+car1.printInfo();
+car1.setSpeed(560);
+car1.setMark("lamba");
+car1.printInfo();
--- test.ts
+++ test.ts
@@ -1,28 +1,36 @@
-let a:number = 123;
-let b:string = 'hello';
-let c:number[] = [1,3345,4,6234,7];
-let d:Array<number> = [1,4,56];
-let aa:number|string;
-let f:boolean = true;
+let a: number = 123;
+let b: string = 'hello';
+let c: number[] = [1, 3345, 4, 6234, 7];
+let d: Array<number> = [1, 4, 56];
+let aa: number | string;
+let f: boolean = true;
 
-class Car{
-    private speed:number;
-    private mark:string;
+class Car {
+    private speed: number;
+    private mark: string;
 
-    constructor(speed:number, mark:string) {
-        this.speed=speed;
-        this.mark=mark;
+    constructor(speed: number, mark: string) {
+        this.speed = speed;
+        this.mark = mark;
     }
 
-    public printInfo(){
+    public printInfo() {
         console.log(`mark = ${this.mark} speed = ${this.speed}`);
     }
 
-    public setSpeed(speed:number){
-        this.speed=speed;
+    public setSpeed(speed: number) {
+        this.speed = speed;
     }
 
-    public setMark(mark:string){
-        this.mark=mark;
+    public setMark(mark: string) {
+        this.mark = mark;
     }
 }
+
+let car1:Car = new Car(140,"ford mustang");
+car1.printInfo();
+
+car1.setSpeed(560);
+car1.setMark("lamba");
+
+car1.printInfo();
